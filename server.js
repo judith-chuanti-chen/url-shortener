06cdfef5a01f557fe7edf55c319a4acee73d3dac
Judith Chuan Ti Chen <chen.chuan@husky.neu.edu>
2020-11-24 15:38:22 -0800

    Updated server.js

--- server.js
+++ server.js
@@ -17,15 +17,15 @@ app.use(express.urlencoded({extended: false}));
 //Define Routes
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
-app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
 
-if (process.env.NODE_ENV === "production"){
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-    });
-}
+// if (process.env.NODE_ENV === "production"){
+//     app.use(express.static("build"));
+//     app.get("*", (req, res) => {
+//       res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+//     });
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
