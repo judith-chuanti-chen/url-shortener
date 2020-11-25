136d7b291aa81848912bb2060b90daf330230dcf
Judith Chuan Ti Chen <chen.chuan@husky.neu.edu>
2020-11-25 11:21:28 -0800

    Bug fix

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({extended: false}));
 app.use('/url', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
 app.use(express.static(path.join(__dirname, 'client/build')));
-app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'../client/public/index.html'));})
+app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'client/public/index.html'));})
 
 if (process.env.NODE_ENV === "production"){
     // app.use(express.static("client/build"));
